96ce067aeb66724bc731bd974f58816242de57c6
NinoB92 <116513269+NinoB92@users.noreply.github.com>
2023-10-29 20:09:40 +0100

    Implement sanctum for API token authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +22,7 @@ Route::get('/', function () {
 
 
 Route::prefix('auth')->group(function (){
-    Route::post('/login', LoginController::class);
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
